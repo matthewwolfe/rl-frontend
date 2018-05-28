72f23ae23adf926ea4308e67ab6a59b1dcb49ce5
matthewwolfe <matthew.wolfe2@gmail.com>
2018-05-28 13:32:17 -0400

    add all colors

--- application/views/application/components/SplashScreen.js
+++ application/views/application/components/SplashScreen.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { Container } from 'reactstrap';
 
 
 function SplashScreen() {
     return (
-        <h1>Loading</h1>
+        <Container
+            className="splash-screen"
+            fluid>
+
+            <h1 className="text-center">
+                RL Trader
+            </h1>
+            <h4 className="text-center">
+                Loading Content
+            </h4>
+        </Container>
     );
 }
 
